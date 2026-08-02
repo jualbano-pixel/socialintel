0e652c4c6c778188a5bd9986670fbc9b19e1c57c
jualbano-pixel <jualbano@gmail.com>
2026-08-02 16:29:54 +0800

    Include Chromium binaries in PDF route trace

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+    outputFileTracingIncludes: {
+      '/api/export-pdf': ['./node_modules/@sparticuz/chromium/bin/**'],
+    },
   },
 };
 module.exports = nextConfig;
